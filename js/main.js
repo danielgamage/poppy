9ead0e84fad93805ec12b6cd60ed721820a81b2f
Daniel Gamage <hello@danielgamage.com>
2013-06-12 13:19:12 -0400

    switching to jquery from tabindex and focus

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
+$(document).ready(function() {
 
+	$('.cover').click(function() {
+		$(this).siblings('.tools').each(function() {
+			if ($(this).hasClass('open')) {
+				$(this).removeClass('open');
+			} else {
+				$(this).addClass('open');
+			}
+		});
+	});
+
+});
