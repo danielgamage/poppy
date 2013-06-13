c6accc578b97845e7abf05c9f9c8f29f319682c3
Daniel Gamage <hello@danielgamage.com>
2013-06-13 09:24:49 -0400

    only one group open at a time

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 
 	$('.cover').click(function() {
+		$('.tools a').removeClass('open');
 		$(this).siblings('.tools').children().each(function() {
 			if ($(this).hasClass('open')) {
 				$(this).removeClass('open');
